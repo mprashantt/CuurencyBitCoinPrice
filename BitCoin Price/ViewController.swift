641f2e6e0d436f9b7eb054a18037be0091d6c815
mprashantt <tprashant4648010@gmail.com>
2019-06-27 13:35:12 +0530

    Podfile added and installed

--- BitCoin Price/ViewController.swift
+++ BitCoin Price/ViewController.swift
@@ -9,12 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let apiURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC"
+    let countryPickerData = ""
+    
+    @IBOutlet weak var countryPicker: UIPickerView!
+    @IBOutlet weak var priceLable: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
 }
 
---
+++ Pods/Target Support Files/Pods-BitCoin Price/Pods-BitCoin Price-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BitCoin_Price : NSObject
+@end
+@implementation PodsDummy_Pods_BitCoin_Price
+@end
